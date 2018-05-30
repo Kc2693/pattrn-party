f7d7041f80918d93bcaab93234b72e7848b5e3b7
Kailin Cannon <kailin.cannon@gmail.com>
2018-05-30 04:57:18 -0600

    whoops first big commit halfway through

---
+++ accordion/accordion-script.js
@@ -0,0 +1,76 @@
+window.onload = function() {
+  accordionTab();
+  fillCodeViews();
+};
+
+document.getElementById('view-accordion-code').addEventListener('click', function() {
+  let codeTab = this.nextElementSibling;
+  if (codeTab.style.display === "none") {
+    codeTab.style.display = "grid"
+  } else {
+    codeTab.style.display = "none"
+  }
+})
+
+var tabBtns = document.querySelectorAll(".tab-button");
+
+const accordionTab = () => {
+  for (let i = 0; i < tabBtns.length; i++) {
+      tabBtns[i].addEventListener("click", function() {
+        let tab = this.nextElementSibling;
+        clearOpenTabs();
+        this.classList.add('active');
+        tab.classList.add('active');
+        tab.style.display = "block";
+      });
+  }
+}
+
+const clearOpenTabs = () => {
+  var tabs = document.querySelectorAll(".tab-content")
+
+  tabs.forEach((tab) => {
+    tab.style.display = "none";
+    tab.classList.remove('active')
+  })
+
+  tabBtns.forEach((btn) => {
+    btn.classList.remove('active')
+  })
+}
+
+const fillCodeViews = () => {
+  let HTMLCode = `<div class="tab-display">
+    <ul class="tabs" role="tablist">
+      <li>
+        <button class="tab-button">Tab 1</button>
+          <div class="tab-content">
+            <p></p>
+          </div>
+      </li>
+      <li>
+        <button name="tabs" class="tab-button">Tab 2</button>
+          <div class="tab-content">
+          </div>
+      </li>
+    </ul>
+  </div>`
+  let jsCode = `const accordionTab = () => {
+    var tabs = document.getElementsByClassName("tab-content")
+
+    for (let i = 0; i < tabBtns.length; i++) {
+        tabBtns[i].addEventListener("click", function() {
+          this.classList.toggle('active')
+
+          let tab = this.nextElementSibling;
+          if (tab.style.display === "block") {
+              tab.style.display = "none";
+          } else {
+              tab.style.display = "block";
+          }
+        });
+    }
+  }`
+  document.getElementById('html-code').innerHTML = HTMLCode;
+  document.getElementById('js-code').innerHTML = jsCode;
+}
